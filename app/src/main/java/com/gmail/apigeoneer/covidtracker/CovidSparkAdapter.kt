3781b4e4de8d3be956f54bc1de1a21085ba74d15
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-21 18:50:51 +0530

    Created enum class for chart interaction.

--- app/src/main/java/com/gmail/apigeoneer/covidtracker/CovidSparkAdapter.kt
+++ app/src/main/java/com/gmail/apigeoneer/covidtracker/CovidSparkAdapter.kt
@@ -6,10 +6,17 @@ class CovidSparkAdapter(
     private val dailyData: List<CovidData>
 ) : SparkAdapter() {
 
+    var metric = Metric.POSITIVE
+    var daysAgo = TimeScale.ALLTIME
+
     override fun getY(index: Int): Float {
         val chosenDayDate = dailyData[index]
         // Default return was Int
-        return chosenDayDate.positiveIncrease.toFloat()
+        return when (metric) {
+            Metric.NEGATIVE -> chosenDayDate.negativeIncrease.toFloat()
+            Metric.POSITIVE -> chosenDayDate.positiveIncrease.toFloat()
+            Metric.DEATH -> chosenDayDate.deathIncrease.toFloat()
+        }
     }
 
     override fun getItem(index: Int) = dailyData[index]
