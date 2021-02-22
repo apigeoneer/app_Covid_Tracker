b8db31279ba707a6f353d505380ef77eaa9fc6a9
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-02-22 20:00:13 +0530

    Added Ticker library & implemented it.

--- app/src/main/java/com/gmail/apigeoneer/covidtracker/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/covidtracker/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.google.gson.GsonBuilder
 import com.robinhood.spark.SparkView
+import com.robinhood.ticker.TickerUtils
+import com.robinhood.ticker.TickerView
+import org.angmarch.views.NiceSpinner
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,11 +22,15 @@ import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-    private const val BASE_URL = "https://api.covidtracking.com/v1/"
-    private const val TAG = "MainActivity"
-
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val BASE_URL = "https://api.covidtracking.com/v1/"
+        private const val TAG = "MainActivity"
+        private const val ALL_STATES = "All (nationwide)"
+    }
+
+    private lateinit var spinnerSelect: NiceSpinner
     private lateinit var currentlyShownData: List<CovidData>
     private lateinit var rgMetricSelection: RadioGroup
     private lateinit var rgTimeSelection: RadioGroup
@@ -32,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rbAllTime: RadioButton
     private lateinit var rbPositive: RadioButton
     private lateinit var tvDateLabel: TextView
-    private lateinit var tvMetricLabel: TextView
+    private lateinit var tickerView: TickerView
     private lateinit var perStateDailyData: Map<String, List<CovidData>>
     private lateinit var nationalDailyData: List<CovidData>
 
@@ -40,12 +47,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        spinnerSelect = findViewById(R.id.spinner_select)
         rgMetricSelection = findViewById(R.id.metric_selection_rg)
         rgTimeSelection = findViewById(R.id.time_selection_rg)
         sparkView = findViewById(R.id.spark_view)
         rbAllTime = findViewById(R.id.all_time_rb)
         rbPositive = findViewById(R.id.positive_rb)
-        tvMetricLabel = findViewById(R.id.metric_label_tv)
+        tickerView = findViewById(R.id.ticker_view)
         tvDateLabel = findViewById(R.id.date_label_tv)
 
         /**
@@ -77,6 +85,8 @@ class MainActivity : AppCompatActivity() {
                     Log.w(TAG, "Didn't receive a valid response body for the National data.")
                     return
                 }
+
+                setupEventListeners()
                 // WE USE REVERSED(), to call the older data first (for graphing purposes)
                 nationalDailyData = nationalData.reversed()
                 Log.i(TAG, "Update graph w/ national data")
@@ -110,13 +120,29 @@ class MainActivity : AppCompatActivity() {
                  */
                 perStateDailyData = statesData.reversed().groupBy { it.state }
                 Log.i(TAG, "Update spinner w/ state names")
-                // TODO: Update graph w/ state data
+                // Update spinner w/ state names
+                updateSpinnerWithStateData(perStateDailyData.keys)
             }
         })
     }
 
+    private fun updateSpinnerWithStateData(stateNames: Set<String>) {
+        val stateAbbreviationList = stateNames.toMutableList()
+        stateAbbreviationList.sort()
+        stateAbbreviationList.add(0, ALL_STATES)
+
+        // Add state list as data source for the spinner
+        spinnerSelect.attachDataSource(stateAbbreviationList)
+        spinnerSelect.setOnSpinnerItemSelectedListener { parent, _, position, _ ->
+            val selectedState = parent.getItemAtPosition(position) as String
+            val selectedData = perStateDailyData[selectedState] ?: nationalDailyData
+            updateDisplayWithData(selectedData)
+        }
+    }
+
     private fun setupEventListeners() {
         /* Here goes all the logic for listening to the event when any of the radio buttons are clicked */
+        tickerView.setCharacterLists(TickerUtils.provideNumberList())
 
         // Enable scrubbing on the chart & add a scrub Listener
         sparkView.isScrubEnabled = true
@@ -156,7 +182,7 @@ class MainActivity : AppCompatActivity() {
         // Adding annotation to make it obv that the variable stores a color
         @ColorInt val colorInt = ContextCompat.getColor(this, colorRes)
         sparkView.lineColor = colorInt
-        tvMetricLabel.setTextColor(colorInt)
+        tickerView.setTextColor(colorInt)
 
         // Update the metric on the adapter
         adapter.metric = metric
@@ -185,7 +211,7 @@ class MainActivity : AppCompatActivity() {
             Metric.DEATH -> covidData.deathIncrease
         }
         // Formatting the no. to include commas & decimals at proper places
-        tvMetricLabel.text = NumberFormat.getInstance().format(numCases)
+        tickerView.text = NumberFormat.getInstance().format(numCases)
         // Formatting the date to a more readable form
         val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
         tvDateLabel.text = outputDateFormat.format(covidData.dateChecked)
